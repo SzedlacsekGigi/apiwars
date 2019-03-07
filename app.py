744c28ec1b3f6293752e1b81463c61cb8694d863
Szedlacsek Lilla <szedlacsek.lilla@gmail.com>
2019-03-07 16:56:13 +0100

    created and linked together html pages

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def homepage():
     return render_template('home.html')
 
+@app.route('/registration')
+def registration_page():
+    return render_template('registration.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(
